788065227a67251e7f3f4ebb53a13e77e37cdf3d
Davis JR <davisjr2000@hotmail.com>
2019-01-04 19:57:55 -0200

    Add Youtube API Script

---
+++ src/actions/episodes.js
@@ -0,0 +1,11 @@
+[
+  { title: "Naul" , id: 'yy8NwjEtEkQ' , number: 1, descripton: "Luan acaba de descobrir que tem um irmão secreto, assim, o observa travando uma batalha com o poderoso Lord Frene e nem imagina como essa batalha irá acabar."},
+  { title:, id: , number: 2 ,descripton:  },
+  { title:, id: , number: 3 ,descripton:  },
+  { title:, id: , number: 4 ,descripton:  },
+  { title:, id: , number: 5 ,descripton:  },
+  { title:, id: , number: 6 ,descripton:  },
+  { title:, id: , number: 7 ,descripton:  },
+  { title:, id: , number: 8 ,descripton:  },
+
+]
