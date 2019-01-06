489fb8372390c4d6b8cb30869b0d8c8ede78e306
Davis JR <davisjr2000@hotmail.com>
2019-01-06 17:33:23 -0200

    Test Landing Image in Production

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
-app.use(require('webpack-dev-middleware')(compiler, {
-      noInfo: true,
-      publicPath: config.output.path
-    }));
 
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import landingImg from '../assets/images/lr-gang.jpeg'
 
 const Landing = () => {
   return (
-    <div className="landing" style={{backgroundImage:`linear-gradient(to bottom, rgba(0,0,0,0) 1%, rgba(0,0,0,1)), url('../assets/images/lr-gang.jpeg')`}}>
+    <div className="landing" style={{backgroundImage:`linear-gradient(to bottom, rgba(0,0,0,0) 1%, rgba(0,0,0,1)), url(${{landingImg}})`}}>
       <a href="#">
         <img src="../assets/images/lr-logo.jpeg" className="lr-logo"/>
       </a>
