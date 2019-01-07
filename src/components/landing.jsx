bf4ff20c8aad6edc814dc8eb3cff524c98155a3d
Davis JR <davisjr2000@hotmail.com>
2019-01-06 23:34:28 -0200

    Change code for production test

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Landing = () => {
   return (
-    <div className="landing" style={{backgroundImage:`linear-gradient(to bottom, rgba(0,0,0,0) 1%, rgba(0,0,0,1)), url('../assets/images/lr-gang.jpeg')`}}>
+    <div className="landing" style={{backgroundImage:`linear-gradient(to bottom, rgba(0,0,0,0) 1%, rgba(0,0,0,1)), url('../public/images/lr-gang.jpeg')`}}>
       <a href="#">
-        <img src="../assets/images/lr-logo.jpeg" className="lr-logo"/>
+        <img src="../public/images/lr-logo.jpeg" className="lr-logo"/>
       </a>
     </div>
   );
