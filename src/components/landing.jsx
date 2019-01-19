41e492b583025e25a16f1ae7a54f2eff49bdffd9
Davis JR <davisjr2000@hotmail.com>
2019-01-19 01:15:02 -0200

    Remove Require

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -4,7 +4,7 @@ const Landing = () => {
   return (
     <div className="landing" style={{backgroundImage:`linear-gradient(to bottom, rgba(0,0,0,0) 1%, rgba(0,0,0,1)), url('../public/images/lr-gang.jpeg')`}}>
       <a href="#">
-        <img src={require("../public/images/lr-logo.jpeg")} className="lr-logo"/>
+        <img src="../public/images/lr-logo.jpeg" className="lr-logo"/>
       </a>
     </div>
   );
