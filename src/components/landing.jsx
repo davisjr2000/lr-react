1e667a658faf16eb3b5b0a3d7bee5cf3159f3e3a
Davis JR <davisjr2000@hotmail.com>
2019-01-19 01:18:10 -0200

    Add Cloudinary Pictures

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Landing = () => {
   return (
-    <div className="landing" style={{backgroundImage:`linear-gradient(to bottom, rgba(0,0,0,0) 1%, rgba(0,0,0,1)), url('../public/images/lr-gang.jpeg')`}}>
+    <div className="landing" style={{backgroundImage:`linear-gradient(to bottom, rgba(0,0,0,0) 1%, rgba(0,0,0,1)), url('https://res.cloudinary.com/dppztxeda/image/upload/v1547867819/lr-gang.jpg')`}}>
       <a href="#">
-        <img src="../public/images/lr-logo.jpeg" className="lr-logo"/>
+        <img src="https://res.cloudinary.com/dppztxeda/image/upload/v1547867811/lr-logo.jpg" className="lr-logo"/>
       </a>
     </div>
   );
